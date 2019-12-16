e6d82b14c13984b02293a44ad011543978fe8829
Satrat <sara.adkins65@gmail.com>
2019-12-15 22:58:42 -0500

    fixing indexing, log fading

--- src/charlieplex_test/charlieplex_test.ino
+++ src/charlieplex_test/charlieplex_test.ino
@@ -7,28 +7,41 @@
    Licensed under a Creative Commons Attribution 4.0 International license: 
    http://creativecommons.org/licenses/by/4.0/
 */
+                  /* *           *           *           * */
+uint8_t Level[12] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }; 
 
-uint8_t Level[12] = {1, 1, 3, 7, 15, 31, 63, 31, 15, 7, 3, 1 };
+uint8_t Order[12] = {0, 4, 8, 1, 5, 2, 3, 7, 11, 6, 10, 9};
 
-uint8_t Order[12] = {0, 1, 2, 5, 4, 3, 8, 7, 6, 9, 10, 11 };
+uint8_t outerLeds[6] = {0,1,2,3,4,5};
+
+uint8_t logLevels[64] = { 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  1,
+                          1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  2,  2,  2,  2,  2,  3,  3,
+                          3,  4,  4,  4,  5,  5,  6,  7,  7,  8,  9, 10, 11, 12, 14, 15, 17,
+                         18, 20, 23, 25, 28, 31, 34, 38, 42, 46, 51, 57, 63};
+
+// 0 4 8 1 5 2 3 7 11 6 10 9
 
 // Timer/Counter1 overflow interrupt
 ISR(TIM1_COMPA_vect) {
-  static uint8_t first, ramp, column, bits, colbit;
+  static uint8_t row, ramp, column, bits, colbit;
   ramp = (ramp+1) & 0x3F;             // Count from 0 to 63
   if (ramp == 0) {
-    bits = 14;//0x07; //00001110                     // All on
-    column = (column + 1) & 0x03;
-    first = column * 3;               // First LED in this column
-    colbit = 1<<(column+0);
+    bits = 0x07; //0000111                    // All on
+    column = (column + 1) & 0x03; // Count from 0 to 3
+    row = column * 3;               //0 3 6 9
+    colbit = 1<<(column+0); // 0b0001 0b0010 0b0100 0b1000
   }
-  if (Level[first] == ramp) bits = bits & 12;//0x06; // 00000110 00001100
-  if (Level[first+1] == ramp) bits = bits & 10;//0x05; // 00000101 00001010
-  if (Level[first+2] == ramp) bits = bits & 6;//0x03; // 00000011 00000110
-  uint8_t mask = colbit - 1;
+
+  //check if check col has reached its ramp
+  if (Level[row] == ramp) bits = bits & 0x06; // 00000110, mask out pos 0
+  if (Level[row+1] == ramp) bits = bits & 0x05; // 00000101, mask out pos 1
+  if (Level[row+2] == ramp) bits = bits & 0x03; // 00000011, mask out pos 2
+  
+  uint8_t mask = colbit - 1; 
+  
   uint8_t outputs = (bits & mask) | (bits & ~mask)<<1;
-  DDRB = outputs | colbit;
-  PORTB = outputs;
+  DDRB = (outputs | colbit) << 1;
+  PORTB = (outputs) << 1;
 }
 
 void setup() {
@@ -40,10 +53,33 @@ void setup() {
   TIMSK = TIMSK | 1<<OCIE1A;          // Compare Match A interrupt
 }
 
+void runRamp(uint8_t numLeds, int8_t startLevel, int8_t endLevel, int32_t rampTime, uint8_t invertInner)
+{
+  int8_t diff = endLevel > startLevel ? 1 : -1;
+  uint32_t msPerLevel = rampTime / (endLevel - startLevel) * diff;
+
+  uint8_t currValue = startLevel;
+  uint8_t currValueLog = logLevels[currValue];
+  while(currValue != endLevel)
+  {
+    currValue += diff;
+    currValueLog = logLevels[currValue];
+    for(uint8_t i = 0; i < numLeds; i++)
+    {
+      Level[Order[outerLeds[i]]] = currValueLog;
+      if(invertInner) Level[Order[outerLeds[i] + 6]] = endLevel > startLevel? logLevels[endLevel - currValue] : logLevels[startLevel - currValue];
+    }
+    delay(msPerLevel);
+  }
+
+}
+
+void fadeProgram()
+{
+  runRamp(6, 0, 63, 2500, 1);
+  runRamp(6, 63, 0, 2500, 1);
+}
+
 void loop () {
-  // Wave demo
-  uint8_t temp = Level[Order[11]];
-  for (int i=11; i>0; i--) Level[Order[i]] = Level[Order[i-1]];
-  Level[Order[0]] = temp;
-  delay(100);
+  fadeProgram();
 }
