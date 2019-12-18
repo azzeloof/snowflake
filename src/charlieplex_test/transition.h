86457ac5281f7bf5d5dc68870cbc68bcc8c95f56
Adam Zeloof <azzeloof@users.noreply.github.com>
2019-12-17 20:16:15 -0500

    Repo Restructuring

--- src/charlieplex_test/transition.h
+++
@@ -1,9 +0,0 @@
-typedef struct transition_t {
-  uint8_t led;
-  uint8_t currValue;
-  uint8_t endValue;
-  int8_t stepDistance;
-  uint32_t stepTime;
-  uint64_t currTime;
-  uint64_t endTime;
-} transition_t;
